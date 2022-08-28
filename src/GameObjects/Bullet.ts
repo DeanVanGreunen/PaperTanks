f6e33f60b4ba01aadb06563631b370df5b5a2c07
Dean Van Greunen <deanvg9000@gmail.com>
2022-08-28 18:23:26 +0200

    player tank working with input, base tank is rendering

---
+++ src/GameObjects/Bullet.ts
@@ -0,0 +1,43 @@
+import GameObject from "../GameObjects";
+import IAgent from "../Agents/IAgent";
+import Game from "../States/Game";
+import ITankRender from "../Tanks/ITankRender";
+import StateMachine from "../StateMachine";
+
+export default class Bullet extends GameObject {
+    x:number;
+    y:number;
+    r:number;
+
+    vx:number;
+    vy:number;
+    constructor(x:number, y:number, r:number, vx:number, vy:number){
+        super();
+        this.init = this.init.bind(this);
+        this.update = this.update.bind(this);
+        this.render = this.render.bind(this);
+
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.vx = vx;
+        this.vy = vy;
+    }
+
+    public async init(){
+    }
+
+    public async update(delta:number){
+        this.x += delta * this.vx;
+        this.y += delta * this.vy;
+    }
+
+    public render(game:Game){
+        let ctx = StateMachine.canvas.getContext("2d");
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
+        ctx.lineWidth = 0.75;
+        ctx.strokeStyle = '#58aff3';
+        ctx.stroke();
+    }
+}
